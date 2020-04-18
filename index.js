5adce902d0608749b856084aaee61c861be6fc3a
Luis Abellan <squarerobin.82@gmail.com>
2020-04-18 20:01:36 +0200

    GET /api/actionsprojects/:id  done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.use(express.json());
 
 server.get("/api/", (req, res) => res.send("API up and running!"));
 
-// POST -done
+// CREATE -  POST /api/projects -
 server.post("/api/projects", (req, res) => {
   if (!req.body.name || !req.body.description) {
     return res.status(400).json({
@@ -18,8 +18,8 @@ server.post("/api/projects", (req, res) => {
 
   projects
     .insert(req.body)
-    .then((post) => {
-      res.status(201).json(post);
+    .then((project) => {
+      res.status(201).json(project);
     })
     .catch((error) => {
       console.log(error);
@@ -29,35 +29,34 @@ server.post("/api/projects", (req, res) => {
     });
 });
 
-// todo
+// UPDATE - POST /api/projects/:id/actions
 server.post("/api/projects/:id/actions", (req, res) => {
-  console.log(req.params.id);
-  const postId = req.params.id;
-  req.body.post_id = postId;
+ 
 
-  if (!req.body.text) {
+
+  if (!req.body.project_id) {
     return res.status(400).json({
-      errorMessage: "Please provide text for the comment.",
+      errorMessage: "Please provide project for the action.",
     });
   }
-  const postComment = projects.findPostComments(req.body.post_id);
+  const projectAction = projects.getProjectActions(req.body.post_id);
 
-  if (postComment.length === 0) {
+  if (projectAction.length === 0) {
     return res.status(404).json({
       message: "The post with the specified ID does not exist.",
     });
   }
 try {
-     projects
-    .insertComment(req.body)
+     actions
+    .insert(req.body)
 
-    .then((comment) => {
-      return res.status(201).json(comment);
+    .then((action) => {
+      return res.status(201).json(action);
     })
     .catch((error) => {
       //console.log(error);
       res.status(404).json({
-        message: "The post with the specified ID does not exist.",
+        message: "The project with the specified ID does not exist.",
       });
     });
 } catch (error) {
@@ -71,7 +70,7 @@ try {
  
 });
 
-//GET /api/projects done
+//GET /api/projects 
 server.get("/api/projects", (req, res) => {
   projects
     .get()
@@ -87,7 +86,7 @@ server.get("/api/projects", (req, res) => {
     });
 });
 
-// GET /api/projects/:id done
+// GET /api/projects/:id 
 server.get("/api/projects/:id", (req, res) => {
   projects
     .get(req.params.id)
@@ -178,6 +177,105 @@ server.put("/api/projects/:id", (req, res) => {
   
   
 })
+
+
+// ACTIONS
+
+
+
+
+// GET /api/projects/:id 
+server.get("/api/actions/:id", (req, res) => {
+  projects
+    .get(req.params.id)
+    .then((project) => {
+      // console.log(post);
+      if (project === null) {
+        return res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      }
+
+      return res.status(200).json(project);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({
+        error: "The post information could not be retrieved.",
+      });
+    });
+});
+
+// DELETE /api/projects/:id 
+server.delete("/api/projects/:id", (req, res) => {
+
+  projects.get(req.params.id).then((project) => {
+    if (project === null) {
+      return res.status(404).json({
+        message: "The post with the specified ID does not exist.",
+      });
+    }
+    
+  });
+
+  projects.remove(req.params.id)
+    .then((project) => {
+        res.status(204).json()
+
+    })
+    .catch((error) => {
+        res.status(500).json({
+            error: "The project could not be removed"
+        })
+
+    })
+
+})
+
+// Update - PUT /api/projects/:id
+server.put("/api/projects/:id", (req, res) => {
+
+    if (!req.body.name || !req.body.description) {
+        return res.status(400).json({
+          errorMessage: "Please provide name and description for the post.",
+        });
+    }
+
+    projects.get(req.params.id)
+    .then((project) => {
+      if (project === null) {
+        return res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+        }
+   
+    })
+    .catch((error) => {
+        console.log(error)
+
+    })
+  
+    projects.update(req.params.id,req.body)
+      .then((project) => {
+        
+              console.log(res)
+
+             return res.status(200).json(project)
+        
+
+  
+      })
+      .catch((error) => {
+          console.log(error)
+         return res.status(500).json({
+            error: "The project information could not be modified."
+          })
+  
+      })
+  
+  
+})
+
 
 
 server.listen(port, () => console.log(`API running on port ${port}`));
